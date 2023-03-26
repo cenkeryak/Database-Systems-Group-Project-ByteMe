ade03647901f700cae0a6cf230c24f77059bac6d
anlsen <119791895+anlsen@users.noreply.github.com>
2023-03-26 13:38:00 +0300

    Update countries.sql

--- step2/countries.sql
+++ step2/countries.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Countries (
 isoCode VARCHAR(10),
 countryName VARCHAR(50) NOT NULL,
-cCode VARCHAR(10),
+cCode VARCHAR(10) NOT NULL,
 PRIMARY KEY (isoCode),
 FOREIGN KEY (cCode) REFERENCES Continents(cCode)
 );
