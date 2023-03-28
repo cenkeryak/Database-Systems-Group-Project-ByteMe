e44b6f8d276c685f20c59995e5ca06ced18c1998
bahadiryazici065 <127677892+bahadiryazici065@users.noreply.github.com>
2023-03-28 21:23:15 +0300

    Update suicide.sql

--- step2/suicide.sql
+++ step2/suicide.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Suicide(
  IsoCode VARCHAR(10) NOT NULL,
  year INTEGER NOT NULL,
- rate FLOAT,
- share FLOAT,
- ratio FLOAT,
+ rate REAL,
+ share REAL,
+ ratio REAL,
  PRIMARY KEY (IsoCode, year),
  FOREIGN KEY (IsoCode) REFERENCES countries(isoCode) ON DELETE CASCADE );
