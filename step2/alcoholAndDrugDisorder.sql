feca9993ca894f64c37c3502668d119cfb646191
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-03-28 22:45:36 +0300

    Update alcoholAndDrugDisorder.sql

--- step2/alcoholAndDrugDisorder.sql
+++ step2/alcoholAndDrugDisorder.sql
@@ -1,6 +1,6 @@
 CREATE TABLE AlcoholAndDrugDisorder(
 CountryIsoCode VARCHAR(10) NOT NULL,
- year INTEGER NOT NULL,
+ year INT NOT NULL,
  alcoholRate float,
  drugRate float,
  PRIMARY KEY (CountryIsoCode, year),
