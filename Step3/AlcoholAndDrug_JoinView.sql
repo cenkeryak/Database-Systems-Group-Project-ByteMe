052a91d681d162d1352e09c9603c6c7bdcd60ec2
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-22 00:20:40 +0300

    Update AlcoholAndDrug_JoinView.sql

--- Step3/AlcoholAndDrug_JoinView.sql
+++ Step3/AlcoholAndDrug_JoinView.sql
@@ -3,4 +3,3 @@ SELECT Drug.CountryIsoCode,Drug.year,Alcohol.alcoholRate,Drug.drugRate
 FROM High_Drug_Rate Drug
 INNER JOIN High_Alcohol_Rate Alcohol ON Drug.CountryIsoCode = Alcohol.CountryIsoCode;
 
-SELECT * from High_AlcoholDrug_Rate
