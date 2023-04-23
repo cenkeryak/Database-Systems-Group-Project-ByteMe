1e140d3ed6b792f7bc0429d54a06a4a3ad8fe6c8
anlsen <119791895+anlsen@users.noreply.github.com>
2023-04-23 13:56:07 +0300

    Create joins-set-lifeparametrics.sql

---
+++ Step3/joins-set-lifeparametrics.sql
@@ -0,0 +1,28 @@
+SELECT lp1.isoCode
+FROM CountriesPossessLifeParametrics lp1
+GROUP BY lp1.isoCode
+HAVING AVG(lifeExpectancy)<60
+
+INTERSECT 
+
+SELECT lp2.isoCode
+FROM CountriesPossessLifeParametrics lp2
+GROUP BY lp2.isoCode
+HAVING AVG(lifeSatisfaction)>6.5;
+
+
+
+
+
+
+
+SELECT lp1.isoCode
+FROM CountriesPossessLifeParametrics lp1
+GROUP BY lp1.isoCode
+HAVING AVG(lifeExpectancy)>80
+
+INTERSECT 
+
+SELECT lp2.isoCode
+FROM CountriesPossessLifeParametrics lp2
+GROUP BY lp2.isoCode;
