dad5050f7e8a00098ed0b5f566a54d6121df3137
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-25 13:02:57 +0300

    Add files via upload

---
+++ Step3/Average_AlcholDrug.sql
@@ -0,0 +1,19 @@
+-- AVERAGE Alcohol of The Countries by years
+Create View Average_Alcohol AS
+Select A2.CountryIsoCode,avg(A2.alcoholRate) as average_alcohol
+FROM alcoholanddrugdisorder A2
+GROUP BY A2.CountryIsoCode;
+
+-- AVERAGE Drug of The Countries by years
+Create View Average_Drug AS
+SELECT A1.CountryIsoCode,avg(A1.drugRate) as average_drug
+FROM alcoholanddrugdisorder A1
+GROUP BY A1.CountryIsoCode;
+
+-- AVERAGE of The Countries by years
+Create View Average_AlcoholDrug AS
+SELECT CountryIsoCode,Average_Alcohol,Average_Drug
+FROM Average_Alcohol
+NATURAL JOIN Average_Drug;
+
+select * From Average_AlcoholDrug;
