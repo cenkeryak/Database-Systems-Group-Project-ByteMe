f616edc3f85d8bcd7326861d75afd03e4b91086f
bahadiryazici065 <127677892+bahadiryazici065@users.noreply.github.com>
2023-04-25 21:28:17 +0300

    Add files via upload

---
+++ Step3/suicide.sql
@@ -0,0 +1,14 @@
+CREATE VIEW HighSuicideRate AS
+SELECT c.countryName, AVG(s.rate) as avg_suicide_rate
+FROM Countries c
+JOIN Suicide s ON c.isoCode = s.IsoCode
+GROUP BY c.isoCode, c.countryName
+ORDER BY avg_suicide_rate DESC;
+
+
+CREATE VIEW LowSuicideRate AS
+SELECT c.countryName, AVG(s.rate) as avg_suicide_rate
+FROM Countries c
+JOIN Suicide s ON c.isoCode = s.IsoCode
+GROUP BY c.isoCode, c.countryName
+ORDER BY avg_suicide_rate ASC;
