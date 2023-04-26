05988e3a5daf6d8af886754f07a7c21a4158032f
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-26 13:05:34 +0300

    view is re-writen
    for better comparison with other views

--- Step3/views-high-alcohol.sql
+++ Step3/views-high-alcohol.sql
@@ -1,5 +1,7 @@
-CREATE VIEW High_Alcohol_Rate AS
-SELECT CountryIsoCode,year,alcoholRate
-FROM alcoholanddrugdisorder
-WHERE alcoholRate > 3;
-
+Create View High_Alcohol_Rate AS
+Select c.countryName,avg(A2.alcoholRate) as average_alcohol
+FROM alcoholanddrugdisorder A2, countries c
+where c.isoCode = A2.CountryIsoCode
+GROUP BY A2.CountryIsoCode
+ORDER BY average_alcohol DESC
+LIMIT 20;
