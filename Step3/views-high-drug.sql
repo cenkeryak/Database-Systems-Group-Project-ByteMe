30ce733d1aee078bcad7d6cb637c9af237ca2dd1
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-26 13:28:24 +0300

    isocode is added

--- Step3/views-high-drug.sql
+++ Step3/views-high-drug.sql
@@ -1,5 +1,5 @@
 Create View High_Drug_Rate AS
-Select c.countryName,avg(A2.drugRate) as average_drug
+Select c.isoCode, c.countryName,avg(A2.drugRate) as average_drug
 FROM alcoholanddrugdisorder A2, countries c
 where c.isoCode = A2.CountryIsoCode
 GROUP BY A2.CountryIsoCode
