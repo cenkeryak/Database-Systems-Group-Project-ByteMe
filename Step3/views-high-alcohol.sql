d4fcbbca063c25dd974fc05bd7a289e9485d13df
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-26 13:27:42 +0300

    country iso code is added

--- Step3/views-high-alcohol.sql
+++ Step3/views-high-alcohol.sql
@@ -1,5 +1,5 @@
 Create View High_Alcohol_Rate AS
-Select c.countryName,avg(A2.alcoholRate) as average_alcohol
+Select c.isoCode,c.countryName,avg(A2.alcoholRate) as average_alcohol
 FROM alcoholanddrugdisorder A2, countries c
 where c.isoCode = A2.CountryIsoCode
 GROUP BY A2.CountryIsoCode
