ebc5ca6430b0ad3a2fd3496941e8c9ed355de0bd
alpay9 <108605025+alpay9@users.noreply.github.com>
2023-04-26 15:58:09 +0300

    Update views-average-alcohol-drug.sql
    countryIsoCode to isoCode, naming convention (>_<*)

--- Step3/views-average-alcohol-drug.sql
+++ Step3/views-average-alcohol-drug.sql
@@ -1,18 +1,18 @@
 -- AVERAGE of years of Alcohol Prevelance of The Countries
 Create View Average_Alcohol AS
-Select A2.CountryIsoCode,avg(A2.alcoholRate) as average_alcohol
+Select A2.isoCode,avg(A2.alcoholRate) as average_alcohol
 FROM alcoholanddrugdisorder A2
-GROUP BY A2.CountryIsoCode;
+GROUP BY A2.isoCode;
 
 -- AVERAGE of years of Drug Prevelance of The Countries
 Create View Average_Drug AS
-SELECT A1.CountryIsoCode,avg(A1.drugRate) as average_drug
+SELECT A1.isoCode,avg(A1.drugRate) as average_drug
 FROM alcoholanddrugdisorder A1
-GROUP BY A1.CountryIsoCode;
+GROUP BY A1.isoCode;
 
 -- AVERAGE of years of Alcohol and Drug Prevelance of The Countries
 Create View Average_AlcoholDrug AS
-SELECT CountryIsoCode,Average_Alcohol,Average_Drug
+SELECT isoCode,Average_Alcohol,Average_Drug
 FROM Average_Alcohol
 NATURAL JOIN Average_Drug;
 
