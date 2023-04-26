c89ba5d94dfd854f2522edc78da4d527ec84717d
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-26 18:24:29 +0300

    Add files via upload

---
+++ Step3/procedure-average-alcohol-drug.sql
@@ -0,0 +1,12 @@
+DELIMITER //
+CREATE PROCEDURE get_avg_alcohol_drug (IN isoCode varchar(5))
+BEGIN 
+	SELECT C.isoCode, C.countryName, AD.Average_Alcohol, AD.Average_Drug
+    FROM Average_AlcoholDrug AD, countries C
+    WHERE C.isoCode = AD.CountryIsoCode AND AD.CountryisoCode = isoCode;
+
+    
+END //
+DELIMITER ;
+
+CALL get_avg_alcohol_drug("TUR");
