c9e85cf4857cf6508655054f0013745b9a41f6c9
alpay9 <108605025+alpay9@users.noreply.github.com>
2023-04-26 15:14:02 +0300

    Add files via upload

---
+++ Step3/views-finance.sql
@@ -0,0 +1,18 @@
+-- DROP VIEW IF EXISTS FinScores;
+CREATE VIEW FinScores AS
+SELECT Countries.countryName, Countries.isoCode, ROUND(AVG(GDP*giniCoefficient),0) as fin
+FROM CountriesHaveFinancialStatus fin_stat
+JOIN Countries ON Countries.IsoCode = fin_stat.isoCode
+GROUP BY Countries.isoCode;
+
+-- DROP VIEW IF EXISTS BestFinCountries;
+CREATE VIEW BestFinCountries AS
+SELECT * FROM FinScores
+ORDER BY fin DESC
+LIMIT 20;
+
+-- DROP VIEW IF EXISTS WorstFinCountries;
+CREATE VIEW WorstFinCountries AS
+SELECT * FROM FinScores
+ORDER BY fin ASC
+LIMIT 20;
