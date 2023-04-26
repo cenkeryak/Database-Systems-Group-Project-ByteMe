108eaf19b0ba2f8f3a2edd7b14f4a6b51c07519a
alpay9 <108605025+alpay9@users.noreply.github.com>
2023-04-26 15:56:59 +0300

    Update views-high-alcohol.sql
    countryIsoCode to isoCode, naming convention (>_<*)

--- Step3/views-high-alcohol.sql
+++ Step3/views-high-alcohol.sql
@@ -1,7 +1,7 @@
 Create View High_Alcohol_Rate AS
 Select c.isoCode,c.countryName,avg(A2.alcoholRate) as average_alcohol
 FROM alcoholanddrugdisorder A2, countries c
-where c.isoCode = A2.CountryIsoCode
-GROUP BY A2.CountryIsoCode
+where c.isoCode = A2.isoCode
+GROUP BY A2.isoCode
 ORDER BY average_alcohol DESC
 LIMIT 20;
