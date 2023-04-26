101b5552500c0f1bc5aac381cdb7bbab76393d03
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-26 11:42:05 +0300

    Comments are updated

--- Step3/Average_AlcholDrug.sql
+++ Step3/Average_AlcholDrug.sql
@@ -1,16 +1,16 @@
--- AVERAGE Alcohol of The Countries by years
+-- AVERAGE of years of Alcohol Prevelance of The Countries
 Create View Average_Alcohol AS
 Select A2.CountryIsoCode,avg(A2.alcoholRate) as average_alcohol
 FROM alcoholanddrugdisorder A2
 GROUP BY A2.CountryIsoCode;
 
--- AVERAGE Drug of The Countries by years
+-- AVERAGE of years of Drug Prevelance of The Countries
 Create View Average_Drug AS
 SELECT A1.CountryIsoCode,avg(A1.drugRate) as average_drug
 FROM alcoholanddrugdisorder A1
 GROUP BY A1.CountryIsoCode;
 
--- AVERAGE of The Countries by years
+-- AVERAGE of years of Alcohol and Drug Prevelance of The Countries
 Create View Average_AlcoholDrug AS
 SELECT CountryIsoCode,Average_Alcohol,Average_Drug
 FROM Average_Alcohol
