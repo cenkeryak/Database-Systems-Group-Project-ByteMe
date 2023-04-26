8cfd013442b4d94fbe110f6356d8fd6188a89c67
Ali Cenker Yakisir <72812171+cenkeryak@users.noreply.github.com>
2023-04-26 13:26:48 +0300

    country isocode is added

--- Step3/views-high-alcohol-drug-prevalence.sql
+++ Step3/views-high-alcohol-drug-prevalence.sql
@@ -1,4 +1,4 @@
 CREATE VIEW High_AlcoholDrug_Rate AS
-SELECT Drug.countryName, Alcohol.average_alcohol, Drug.average_drug
+SELECT Drug.isoCode, Drug.countryName, Alcohol.average_alcohol, Drug.average_drug
 FROM High_Drug_Rate Drug
-INNER JOIN High_Alcohol_Rate Alcohol ON Drug.countryName = Alcohol.countryName;
+INNER JOIN High_Alcohol_Rate Alcohol ON Drug.isoCode = Alcohol.isoCode;
